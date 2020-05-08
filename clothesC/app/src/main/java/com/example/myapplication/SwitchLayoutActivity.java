f58cdb0d241766e8060391a5b6c7c15b791b81de
geumkyeong <ansrmarud@gmail.com>
2020-05-08 18:49:20 +0900

    Grid 레이아웃, Recycler 레이아웃 간 전환_주석_추가
    그리드, 리사이클러 레이아웃 전환. 전에 커밋한 소스파일에 주석을 달아 올립니다. 업로드 파일에 대한 설명은 issues에 올렸습니다!

--- clothesC/app/src/main/java/com/example/myapplication/SwitchLayoutActivity.java
+++ clothesC/app/src/main/java/com/example/myapplication/SwitchLayoutActivity.java
@@ -26,17 +26,22 @@ public class SwitchLayoutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_switch_layout);
 
+        // SVG 사용
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
 
+        // items에 item들 할당
         initItemsData();
 
+        // 객체 생성
         gridLayoutManager = new GridLayoutManager(this, SPAN_COUNT_ONE);
         itemAdapter = new ItemViewer(items, gridLayoutManager);
+        // recyclerview 가져옴
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        recyclerView.setAdapter(itemAdapter);
-        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setAdapter(itemAdapter);               // ItemViewer를 어댑터로 연결
+        recyclerView.setLayoutManager(gridLayoutManager);   // GridLayoutManger 사용 선언
     }
 
+//  item 요소(해당 이미지, 제목, 설명 등) 설정한 뒤 items(배열)에 추가하는 메소드.
     private void initItemsData() {
         items = new ArrayList<>(4);
         items.add(new Item(R.drawable.img1, "Image 1", 20, 33));
@@ -51,25 +56,29 @@ public class SwitchLayoutActivity extends AppCompatActivity {
         return true;
     }
 
+//  레이아웃 전환 아이콘 선택 시 동작을 처리하는 메소드.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.menu_switch_layout) {
-            switchLayout();
-            switchIcon(item);
+            switchLayout();     // 레이아웃 전환
+            switchIcon(item);   // 아이콘 바꾸기
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+// 한 줄에 표시되는 아이템 수에 따라 레이아웃을 전환하는 메소드.
     private void switchLayout() {
         if (gridLayoutManager.getSpanCount() == SPAN_COUNT_ONE) {
             gridLayoutManager.setSpanCount(SPAN_COUNT_THREE);
         } else {
             gridLayoutManager.setSpanCount(SPAN_COUNT_ONE);
         }
+
         itemAdapter.notifyItemRangeChanged(0, itemAdapter.getItemCount());
     }
 
+//  레이아웃 전환 시 아이콘을 바꾸는 메소드.
     private void switchIcon(MenuItem item) {
         if (gridLayoutManager.getSpanCount() == SPAN_COUNT_THREE) {
             item.setIcon(getResources().getDrawable(R.drawable.ic_span_3));
