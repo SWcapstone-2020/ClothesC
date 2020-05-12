fe07c9b0487f200dedb3504bd478ffc7541d4fb9
Choi33 <choi333ys@gmail.com>
2020-05-12 09:30:05 +0900

    프로필 사진 수정 기능 추가

--- clothesC/app/src/main/java/com/example/myapplication/ProfileUpdateActivity.java
+++ clothesC/app/src/main/java/com/example/myapplication/ProfileUpdateActivity.java
@@ -1,33 +1,48 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
-import com.example.myapplication.signup.LoginActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.InputStream;
 
 public class ProfileUpdateActivity extends AppCompatActivity {
+    ImageView renewProfile;
+    private FirebaseStorage storage;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private  final int GALLERY_CODE = 10;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profileupdate);
-
+        renewProfile=findViewById(R.id.renewProfileImge);
 
         findViewById(R.id.updateButton).setOnClickListener(onClickListener);
-
-
+        findViewById(R.id.renewProfileImge).setOnClickListener(onClickListener);
 
     }
     View.OnClickListener onClickListener = new View.OnClickListener() {
@@ -39,7 +54,10 @@ public class ProfileUpdateActivity extends AppCompatActivity {
                     startActivity(ProfileActivity.class);
                     finish();
                     break;
-
+                case R.id.renewProfileImge:
+//                    updateImage();
+                    loadAlbum();
+                    break;
             }
         }
     };
@@ -47,8 +65,6 @@ public class ProfileUpdateActivity extends AppCompatActivity {
     private void updateProfile(){
         String renewalName=((TextView)findViewById(R.id.updateName)).getText().toString();
         String renewalBirth=((TextView)findViewById(R.id.updateBirth)).getText().toString();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
         DocumentReference users = db.collection("users").document(user.getUid());
 
         users.update("name",renewalName, "birth", renewalBirth)
@@ -67,6 +83,95 @@ public class ProfileUpdateActivity extends AppCompatActivity {
 
     }
 
+/*    private void updateImage(){
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://clothesc-ver1.appspot.com");
+        StorageReference desertRef = storageRef.child("profileImage/" + user.getUid()+"/"+"profile.png");
+
+        desertRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                startToast("삭제성공");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                startToast("삭제실패");
+            }
+        });
+    }*/
+    private void loadAlbum(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, GALLERY_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://clothesc-ver1.appspot.com");
+        if (requestCode == GALLERY_CODE) {
+            StorageReference desertRef = storageRef.child("profileImage/" + user.getUid()+"/"+"profile.png");
+            desertRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    startToast("삭제성공");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    startToast("삭제실패");
+                }
+            });
+            Uri file = Uri.fromFile(new File(getPath(data.getData())));
+            String profile=file.getLastPathSegment();
+            profile="profile.png";
+            StorageReference riversRef = storageRef.child("profileImage/" + user.getUid()+"/"+profile);
+            UploadTask uploadTask = riversRef.putFile(file);
+
+            try {
+                // 선택한 이미지에서 비트맵 생성
+                InputStream in = getContentResolver().openInputStream(data.getData());
+
+                Bitmap img = BitmapFactory.decodeStream(in);
+                in.close();
+                // 이미지 표시
+                renewProfile.setImageBitmap(img);
+                //영상코드
+                //profilPath = data.getStringExtra("profilePath");
+                //Bitmap bmp = BitmapFactory.decorderFile(profilePath);
+                // profileImageView.setImageBitmap(bmp);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                }
+            });
+
+
+        }
+    }
+
+    public String getPath(Uri uri){
+        String [] proj={MediaStore.Images.Media.DATA};
+        CursorLoader cursorLoader=new CursorLoader(this,uri,proj,null,null,null);
+
+        Cursor cursor=cursorLoader.loadInBackground();
+        int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(index);
+
+    }
     private void startToast(String msg){
         Toast.makeText(this, msg,Toast.LENGTH_SHORT).show();
     }
