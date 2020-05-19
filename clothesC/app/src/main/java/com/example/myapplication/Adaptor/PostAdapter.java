25215e6b9421d435d7e1e0d35ad381e8b9c394c9
Choi33 <choi333ys@gmail.com>
2020-05-19 11:54:37 +0900

    게시글 출력

--- clothesC/app/src/main/java/com/example/myapplication/Adaptor/PostAdapter.java
+++ clothesC/app/src/main/java/com/example/myapplication/Adaptor/PostAdapter.java
@@ -1,19 +1,25 @@
 package com.example.myapplication.Adaptor;
 
 import android.app.Activity;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.myapplication.PostInfo;
 import com.example.myapplication.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.GalleryViewHolder> {
     private ArrayList<PostInfo> mDataset;
@@ -21,9 +27,34 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.GalleryViewHol
 
     static class GalleryViewHolder extends RecyclerView.ViewHolder {
         CardView cardView;
-        GalleryViewHolder(CardView v) {
+
+        GalleryViewHolder(Activity activity, CardView v, PostInfo postInfo) {
             super(v);
             cardView = v;
+
+            LinearLayout contentsLayout = cardView.findViewById(R.id.contentLayout);
+            ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            ArrayList<String> contentList = postInfo.getContents();
+
+            if (contentsLayout.getChildCount() == 0) {
+                for (int i = 0; i < contentList.size(); i++) {
+                    String contents = contentList.get(i);
+                    if (Patterns.WEB_URL.matcher(contents).matches()) { //내용이 url인가? (즉 이미지인가 동영상인가)
+                        ImageView imageView = new ImageView(activity);
+                        imageView.setLayoutParams(layoutParams);
+                        imageView.setAdjustViewBounds(true);
+                        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                        contentsLayout.addView(imageView);
+//                        Glide.with(activity).load(contents).override(1000).thumbnail(0.1f).into(imageView);
+                    } else { //텍스트인가
+                        TextView textView = new TextView(activity);
+                        textView.setLayoutParams(layoutParams);
+//                        textView.setText(contents);
+                        contentsLayout.addView(textView);
+
+                    }
+                }
+            }
         }
     }
 
@@ -32,11 +63,17 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.GalleryViewHol
         this.activity = activity;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+
     @NonNull
     @Override
     public PostAdapter.GalleryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         CardView cardView = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_post, parent, false);
-        final GalleryViewHolder galleryViewHolder = new GalleryViewHolder(cardView);
+        final GalleryViewHolder galleryViewHolder = new GalleryViewHolder(activity, cardView, mDataset.get(viewType));
         cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,12 +86,27 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.GalleryViewHol
     @Override
     public void onBindViewHolder(@NonNull final GalleryViewHolder holder, int position) {
         CardView cardView = holder.cardView;
-        TextView titleTextView=cardView.findViewById(R.id.titleTextView);
+
+        //제목 출력
+        TextView titleTextView = cardView.findViewById(R.id.titleTextView);
         titleTextView.setText(mDataset.get(position).getTitle());
 
-        TextView createdAtText=cardView.findViewById(R.id.createAtTextView);
-        createdAtText.setText(mDataset.get(position).getCreatedAt().toString());
+        //작성날짜 출력
+        TextView createdAtText = cardView.findViewById(R.id.createAtTextView);
+        createdAtText.setText(new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(mDataset.get(position).getCreatedAt()));
 
+        //내용 출력
+        LinearLayout contentsLayout = cardView.findViewById(R.id.contentLayout);
+        ArrayList<String> contentList = mDataset.get(position).getContents();
+
+        for (int i = 0; i < contentList.size(); i++) {
+            String contents = contentList.get(i);
+            if (Patterns.WEB_URL.matcher(contents).matches()) { //내용이 url인가? (즉 이미지인가 동영상인가)
+                Glide.with(activity).load(contents).override(1000).thumbnail(0.1f).into((ImageView) contentsLayout.getChildAt(i));
+            } else { //텍스트인가
+                ((TextView) contentsLayout.getChildAt(i)).setText(contents);
+            }
+        }
     }
 
     @Override
--- clothesC/app/src/main/java/com/example/myapplication/ShowPostActivity.java
+++ clothesC/app/src/main/java/com/example/myapplication/ShowPostActivity.java
@@ -10,11 +10,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.Adaptor.PostAdapter;
+import com.example.myapplication.signup.LoginActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -22,44 +27,83 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class ShowPostActivity extends AppCompatActivity {
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore firebaseFirestore;
+    private RecyclerView recyclerView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_post);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        if(firebaseUser==null){
+            startActivity(LoginActivity.class);
+        }
+        else{
 
-
-        db.collection("posts")
-
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    ArrayList<PostInfo> postList=new ArrayList<>();;
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                postList.add(new PostInfo(document.getData().get("title").toString(),
-                                        (ArrayList<String>) document.getData().get("contents"),
-                                        (ArrayList<String>) document.getData().get("formats"),
-                                        document.getData().get("publisher").toString(),
-                                        new Date(document.getDate("createdAt").getTime())
-                                ));
+            DocumentReference documentReference=firebaseFirestore.collection("users").document(firebaseUser.getUid());
+            documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if(task.isSuccessful()){
+                        DocumentSnapshot document=task.getResult();
+                        if(document != null){
+                            if(document.exists()){
+//                                Log.d("show","Document data:"+document.getData());
                             }
-                            RecyclerView recyclerView = findViewById(R.id.recyclerView);
-                            recyclerView.setHasFixedSize(true);
-                            recyclerView.setLayoutManager(new LinearLayoutManager(ShowPostActivity.this));
-
-                            RecyclerView.Adapter mAdapter = new PostAdapter(ShowPostActivity.this, postList);;
-                            recyclerView.setAdapter(mAdapter);
-                        } else {
-
+                            else{
+                                startActivity(LoginActivity.class);
+                            }
                         }
                     }
-
-                });
+                    else{
+//                        Log.d("show","get failed with"+task.getException());
+                    }
+                }
+            });
+        }
+        recyclerView = findViewById(R.id.recyclerView);
         findViewById(R.id.writePost).setOnClickListener(onClickListener);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(ShowPostActivity.this));
+
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        if(firebaseUser!=null){
+            CollectionReference collectionReference=firebaseFirestore.collection("posts");
+            collectionReference.orderBy("createdAt", Query.Direction.DESCENDING).get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        ArrayList<PostInfo> postList=new ArrayList<>();;
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    postList.add(new PostInfo(document.getData().get("title").toString(),
+                                            (ArrayList<String>) document.getData().get("contents"),
+                                            (ArrayList<String>) document.getData().get("formats"),
+                                            document.getData().get("publisher").toString(),
+                                            new Date(document.getDate("createdAt").getTime())
+                                    ));
+                                }
+
+
+                                RecyclerView.Adapter mAdapter = new PostAdapter(ShowPostActivity.this, postList);;
+                                recyclerView.setAdapter(mAdapter);
+                            } else {
+
+                            }
+                        }
+
+                    });
+        }
+
+
 
 
     }
