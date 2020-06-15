989d25e232f27b17a5d133abfe6d5adb8529475b
Choi33 <choi333ys@gmail.com>
2020-06-15 17:32:45 +0900

    본인이 작성한 게시글 출력

---
+++ clothesC/app/src/main/java/com/example/myapplication/Adaptor/MyPostAdapter.java
@@ -0,0 +1,161 @@
+package com.example.myapplication.Adaptor;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.myapplication.Post.DetailPostActivity;
+import com.example.myapplication.Post.PostInfo;
+import com.example.myapplication.R;
+import com.example.myapplication.listener.OnPostListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static com.example.myapplication.Util.isStorageUrl;
+
+public class MyPostAdapter extends RecyclerView.Adapter<MyPostAdapter.PostViewHolder> {
+    private ArrayList<PostInfo> mDataset;
+    private Activity activity;
+    private OnPostListener onPostListener;
+
+    static class PostViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+
+        PostViewHolder(CardView v) {
+            super(v);
+            cardView = v;
+        }
+    }
+
+    public MyPostAdapter(Activity activity, ArrayList<PostInfo> myDataset) {
+        this.mDataset = myDataset;
+        this.activity = activity;
+    }
+
+    public void setOnPostListener(OnPostListener onPostListener){
+        this.onPostListener=onPostListener;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+
+    @NonNull
+    @Override
+    public PostViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        CardView cardView = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.item_post, parent, false);
+        final PostViewHolder postViewHolder = new PostViewHolder(cardView);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity, DetailPostActivity.class);
+                intent.putExtra("postInfo", mDataset.get(postViewHolder.getAdapterPosition()));
+                activity.startActivity(intent);
+            }
+        });
+
+        cardView.findViewById(R.id.menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopup(v,postViewHolder.getAdapterPosition());
+            }
+        });
+
+        return postViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final PostViewHolder holder, int position) {
+        CardView cardView = holder.cardView;
+
+        TextView titleTextView = cardView.findViewById(R.id.titleTextView);
+        titleTextView.setText(mDataset.get(position).getTitle());
+
+        TextView createdAtText = cardView.findViewById(R.id.createAtTextView);
+        createdAtText.setText(new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(mDataset.get(position).getCreatedAt()));
+
+        LinearLayout contentsLayout = cardView.findViewById(R.id.contentLayout);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        ArrayList<String> contentList = mDataset.get(position).getContents();
+
+        if(contentsLayout.getTag()==null || !contentsLayout.equals(contentList)){
+            contentsLayout.setTag(contentList);
+            contentsLayout.removeAllViews();
+            final int MORE_NEED=2;
+            for (int i = 0; i < contentList.size(); i++) {
+                if(i==MORE_NEED){
+                    TextView textView = new TextView(activity);
+                    textView.setLayoutParams(layoutParams);
+                    textView.setText("더보기...");
+                    contentsLayout.addView(textView);
+                    break;
+                }
+                String contents = contentList.get(i);
+                if (isStorageUrl(contents)) {
+                    ImageView imageView = new ImageView(activity);
+                    imageView.setLayoutParams(layoutParams);
+                    imageView.setAdjustViewBounds(true);
+                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+                    contentsLayout.addView(imageView);
+                    Glide.with(activity).load(contents).override(1000).thumbnail(0.1f).into(imageView);
+                } else { //텍스트인가
+                    TextView textView = new TextView(activity);
+                    textView.setLayoutParams(layoutParams);
+                    textView.setText(contents);
+                    textView.setTextColor(Color.rgb(0,0,0)); //텍스트 내용 색깔 지정
+                    contentsLayout.addView(textView);
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public void showPopup(View v, final int position) { //수정, 삭제 메뉴 뜨게함
+        PopupMenu popup = new PopupMenu(activity, v);
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.modify:
+                        onPostListener.onModify(position);
+                        return true;
+                    case R.id.delete:
+                        onPostListener.onDelete(position);
+                        return true;
+                    default:
+                        return false;
+                }
+
+            }
+        });
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.post, popup.getMenu());
+        popup.show();
+    }
+
+
+
+}
--- clothesC/app/src/main/java/com/example/myapplication/fragment/ProfileFragment.java
+++ clothesC/app/src/main/java/com/example/myapplication/fragment/ProfileFragment.java
@@ -10,13 +10,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.myapplication.Adaptor.MyPostAdapter;
 import com.example.myapplication.MainActivity;
+import com.example.myapplication.Post.PostInfo;
+import com.example.myapplication.Post.WritePostActivity;
 import com.example.myapplication.R;
+import com.example.myapplication.listener.OnPostListener;
 import com.example.myapplication.signup.LoginActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -24,15 +31,33 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import static com.example.myapplication.Util.isStorageUrl;
+import static com.example.myapplication.Util.storageUrlToName;
 
 public class ProfileFragment extends Fragment {
     private static final String TAG = "ProfileFragment";
     ImageView imageView;
+    private boolean topScrolled;
+    private FirebaseFirestore db;
+    private ArrayList<PostInfo> postList;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private MyPostAdapter myPostAdapter;
+    private StorageReference storageRef;
+    private Context context;
+    private boolean updating;
+    private int successCount;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -49,12 +74,14 @@ public class ProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        imageView=(ImageView) view.findViewById(R.id.profileView);
-        final TextView textName=(TextView)view.findViewById(R.id.nameEditText);
-        final TextView textBirth=(TextView)view.findViewById(R.id.introEditText);
+        imageView = (ImageView) view.findViewById(R.id.profileView);
+        final TextView textName = (TextView) view.findViewById(R.id.nameEditText);
+        final TextView textBirth = (TextView) view.findViewById(R.id.introEditText);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final RecyclerView recyclerView = view.findViewById(R.id.mypost);
 
+       db = FirebaseFirestore.getInstance();
+        postList = new ArrayList<>();
 
         if (user != null) {
             db.collection("users").document(user.getUid()).get()
@@ -72,13 +99,56 @@ public class ProfileFragment extends Fragment {
                         public void onComplete(@NonNull Task<DocumentSnapshot> task) { //성공시 출력
                         }
                     });
-        }
-        else{
+        } else {
             startActivity(LoginActivity.class);
         }
 
+
         view.findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
         view.findViewById(R.id.profileUpdate).setOnClickListener(onClickListener);
+
+        myPostAdapter=new MyPostAdapter(getActivity(),postList);
+        myPostAdapter.setOnPostListener(onPostListener);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(myPostAdapter);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+                int firstVisibleItemPosition = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
+
+                if (newState == 1 && firstVisibleItemPosition == 0) {
+                    topScrolled = true;
+                }
+                if (newState == 0 && topScrolled) {
+                    postsUpdate(true);
+                    topScrolled = false;
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+                int totalItemCount = layoutManager.getItemCount();
+                int firstVisibleItemPosition = ((LinearLayoutManager) layoutManager).findFirstVisibleItemPosition();
+                int lastVisibleItemPosition = ((LinearLayoutManager) layoutManager).findLastVisibleItemPosition();
+
+                if (totalItemCount - 3 <= lastVisibleItemPosition && !updating) {
+                    postsUpdate(false);
+                }
+
+                if (0 < firstVisibleItemPosition) {
+                    topScrolled = false;
+                }
+            }
+        });
+
+        postsUpdate(false);
 
         return view;
     }
@@ -87,7 +157,7 @@ public class ProfileFragment extends Fragment {
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            switch (v.getId()){
+            switch (v.getId()) {
                 case R.id.logoutButton:
                     FirebaseAuth.getInstance().signOut();
                     startActivity(LoginActivity.class);
@@ -102,8 +172,8 @@ public class ProfileFragment extends Fragment {
     };
 
 
-    private void startActivity(Class c){
-        Intent intent=new Intent(getActivity(),c);
+    private void startActivity(Class c) {
+        Intent intent = new Intent(getActivity(), c);
         startActivity(intent);
     }
 
@@ -113,11 +183,11 @@ public class ProfileFragment extends Fragment {
 
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageReference = storage.getReferenceFromUrl("gs://clothesc-ver1.appspot.com");
-        StorageReference pathReference = storageReference.child("profileImage/"+user.getUid()+"/"+"profile.PNG");
+        StorageReference pathReference = storageReference.child("profileImage/" + user.getUid() + "/" + "profile.PNG");
         pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
-                Log.d(TAG,uri.toString());
+                Log.d(TAG, uri.toString());
 
                 Glide.with(context).load(uri).into(imageView);
 
@@ -135,12 +205,101 @@ public class ProfileFragment extends Fragment {
         super.onDetach();
     }
 
+    private void postsUpdate(final boolean clear) {
+        updating = true;
+        Date date = postList.size() == 0 || clear ? new Date() : postList.get(postList.size() - 1).getCreatedAt();
+        CollectionReference collectionReference = db.collection("posts");
+        collectionReference.orderBy("createdAt", Query.Direction.DESCENDING).whereLessThan("createdAt", date).limit(10).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            if (clear) {
+                                postList.clear();
+                            }
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String id = document.get("publisher").toString();
+                                if (user.getUid().equals(id)) {
+                                    postList.add(new PostInfo(
+                                            document.getData().get("title").toString(),
+                                            (ArrayList<String>) document.getData().get("contents"),
+                                            (ArrayList<String>) document.getData().get("formats"),
+                                            document.getData().get("publisher").toString(),
+                                            new Date(document.getDate("createdAt").getTime()),
+                                            document.getId()));
+                                }
 
-/*    private void renew(){
-        FragmentTransaction ft = getFragmentManager().beginTransaction();
-        ft.detach(this).attach(this).commit();
+                            }
+                            myPostAdapter.notifyDataSetChanged();
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                        updating = false;
+                    }
+                });
+    }
 
-    }*/
+    OnPostListener onPostListener = new OnPostListener() {
+        @Override
+        public void onDelete(int position) {
+            final String id = postList.get(position).getId();
+            ArrayList<String> contentList = postList.get(position).getContents();
+            for (int i = 0; i < contentList.size(); i++) {
+                String contents = contentList.get(i);
+                if (isStorageUrl(contents)) { //내용이 url인가? (즉 이미지인가 동영상인가)
+                    successCount++;
+                    StorageReference desertRef = storageRef.child("posts/" + id + "/" + storageUrlToName(contents));
+                    desertRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(context, "이미지 삭제...", Toast.LENGTH_LONG).show();
+                            successCount--;
+                            storageDeleteUpdate(id);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+//                            showToast(ShowPostActivity.this, "삭제하지 못했습니다");
+                        }
+                    });
 
+                }
+            }
+            storageDeleteUpdate(id);
+        }
+
+        @Override
+        public void onModify(int position) {
+            goWriteActivity(WritePostActivity.class, postList.get(position));
+        }
+
+    };
+
+    private void storageDeleteUpdate(String id) {
+        if (successCount == 0) {
+            db.collection("posts").document(id)
+                    .delete()
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(context, "게시글이 삭제되었습니다.", Toast.LENGTH_LONG).show();
+                            boolean clear = true;
+                            postsUpdate(clear);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(context, "게시글 삭제 실패", Toast.LENGTH_LONG).show();
+                        }
+                    });
+        }
+    }
+
+    private void goWriteActivity(Class c, PostInfo postInfo) {
+        Intent intent = new Intent(getActivity(), c);
+        intent.putExtra("postInfo", postInfo);
+        startActivity(intent);
+    }
 
 }
