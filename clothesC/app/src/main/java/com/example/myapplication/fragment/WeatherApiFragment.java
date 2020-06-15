158eba709dedb2660a60e5cbe2ea9baadd968656
geumkyeong <ansrmarud@gmail.com>
2020-06-15 16:52:23 +0900

    Json Parsing 재업
    코드 수정

--- clothesC/app/src/main/java/com/example/myapplication/fragment/WeatherApiFragment.java
+++ clothesC/app/src/main/java/com/example/myapplication/fragment/WeatherApiFragment.java
@@ -47,6 +47,8 @@ public class WeatherApiFragment extends Fragment {
     TextView mTextView;
     ProgressDialog pd;
 
+    myAsyncTask currentTask;
+    BackgroundTask dayTask;
     public WeatherApiFragment() {
 
     }
@@ -64,44 +66,27 @@ public class WeatherApiFragment extends Fragment {
         setHasOptionsMenu(true);
         mTextView = (TextView) rootView.findViewById(R.id.weatherTextview);
 
-        myAsyncTask mat = new myAsyncTask(); //myAsyncTask 객체 생성
-        mat.execute(type1); //myAsyncTask 실행하기
+        //새로 정의한 BackgruondTask 객체 생성
+        currentTask = new myAsyncTask();
+        //Async스레드를 시작
+        currentTask.execute(type1);
 
-        dAsyncTask mat2 = new dAsyncTask(); //myAsyncTask 객체 생성
-        mat2.execute(type2); //myAsyncTask 실행하기
+
 
         return rootView;
     }
 
-
-    protected class myAsyncTask extends AsyncTask<String, Void, Boolean> {
+// 초단기 예보: 현재 날씨
+    protected class myAsyncTask extends AsyncTask<String, Void, ArrayList<HashMap<String, String>>> {
         String forecastJsonStr = null;
         ArrayList<HashMap<String, String>> mArrayList = new ArrayList<>();
 
-        protected void saveNshow(ArrayList<HashMap<String, String>> mArrayList, int n, String category, String fcstDate, String fcstTime, String fcstValue) {
-            HashMap<String, String> hashMap = new HashMap<>();
-            hashMap.put(TAG_ID, String.valueOf(n));
-            hashMap.put(TAG_CAT, category);
-            hashMap.put(TAG_DATE, fcstDate);
-            hashMap.put(TAG_TIME, fcstTime);
-            hashMap.put(TAG_VAL, fcstValue);
-
-            mArrayList.add(hashMap);
-
-            HashMap<String, String> outputHashMap = mArrayList.get(n);
-            String id = outputHashMap.get("id");
-            String cat = outputHashMap.get("category");
-            String date = outputHashMap.get("fcstDate");
-            String time = outputHashMap.get("fcstTime");
-            String val = outputHashMap.get("fcstValue");
-
-            String s = String.format(getResources().getString(R.string.textview_message), time, cat, val);
-            mTextView.append(s);
-            Log.d("mArrayList :", String.valueOf(mArrayList.get(n)));
-        }
-
         @Override
-        protected Boolean doInBackground(String... types) {
+        protected ArrayList<HashMap<String, String>> doInBackground(String... types) {
+
+            try { Thread.sleep(100 * 1); }
+            catch (InterruptedException e) { e.printStackTrace(); }
+
             for(String type : types) {
                 HttpURLConnection urlConnection = null; //HttpUrlConnection
                 BufferedReader reader = null;
@@ -113,9 +98,9 @@ public class WeatherApiFragment extends Fragment {
                             String ny = "38";
                             String dataType = "JSON";
                             String key = "8Gnkss2kI%2F%2BVSjVNBRpY9QcW53gpQ953TJoA2E%2B1260r6hNjBYOBotBXFuhE3XXfy2Hzx%2BSV9JdIsoEriCzApQ%3D%3D";
-                            String baseDate = "20200614";
+                            String baseDate = "20200615";
 
-                            String baseTime = "2104";//현재시간
+                            String baseTime = "1700";//현재시간
                             int hour = Integer.valueOf(baseTime.substring(0, 2));
                             int min = Integer.valueOf(baseTime.substring(2, 4));
 //                            min = 0;
@@ -139,16 +124,7 @@ public class WeatherApiFragment extends Fragment {
                     try {
 
                         Log.d("Json Result =>", forecastJsonStr);
-                        JSONObject JsonObj = new JSONObject(forecastJsonStr);
-                        Log.d("JSON", forecastJsonStr);
-                        JSONObject parse_response = JsonObj.getJSONObject("response");
-                        JSONObject parse_body = parse_response.getJSONObject("body");
-                        JSONObject parse_items = parse_body.getJSONObject("items");
-                        //JSONArray parse_item = (JSONArray) parse_items.get("item");
-                        JSONArray parse_item = parse_items.getJSONArray("item");
-
-
-                        currentWeatherParsing(parse_item);
+                        currentWeatherParsing(forecastJsonStr);
 
 
 
@@ -174,9 +150,13 @@ public class WeatherApiFragment extends Fragment {
 
                 }
             }
-            return null;
-        }
 
+
+            return mArrayList;
+        }
+    /**
+     * 원격으로부터 JSON형태의 문서를 받기
+     */
         private void jsonParsing(String type, HttpURLConnection urlConnection, BufferedReader reader, String nx, String ny, String baseDate, String baseTime, String dataType, int numOfRows, int pageNo, String key) throws IOException {
 
             //새 URL 객체
@@ -216,11 +196,12 @@ public class WeatherApiFragment extends Fragment {
             //InputStream 을 사용해 데이터 읽어들이기
             InputStream inputStream = urlConnection.getInputStream();
 
-            //StringBuffer 에 데이터 저장
+
             StringBuffer buffer = new StringBuffer(); // 새로운 StringBuffer 생성
             reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
             String line;
 
+            //주어진 URL 문서의 소스를 줄단위로 읽어(line), buffer에 저장함
             while ((line = reader.readLine()) != null) {
                 buffer.append(line + "\n");
             }
@@ -228,60 +209,71 @@ public class WeatherApiFragment extends Fragment {
                 // 불러온 데이터가 비어있음.
                 forecastJsonStr = null;
             }
-            forecastJsonStr = buffer.toString(); //로드한 데이터 문자열 변수에 저장.
+
+            forecastJsonStr = buffer.toString();
         }
 
-        private void currentWeatherParsing(JSONArray parse_item) throws JSONException {
-            int n = -1;
+    /**
+     * JSON 객체를 생성한 다음에 객체에서 필요한 데이터 추출
+     */
+        private void currentWeatherParsing(String forecastJsonStr) throws JSONException {
+
+            //읽어들인 JSON포맷의 데이터를 JSON객체로 변환
+            JSONObject JsonObj = new JSONObject(forecastJsonStr);
+            Log.d("JSON", forecastJsonStr);
+            JSONObject parse_response = JsonObj.getJSONObject("response");
+            JSONObject parse_body = parse_response.getJSONObject("body");
+            JSONObject parse_items = parse_body.getJSONObject("items");
+            //item의 값은 배열로 구성 되어있으므로 JSON 배열생성
+            JSONArray parse_item = parse_items.getJSONArray("item");
+
+
             JSONObject obj = parse_item.getJSONObject(0);
-            String bt = obj.getString("baseTime"); //1906
+            String bt = obj.getString("baseTime"); //1900
             int hour = Integer.valueOf(bt.substring(0, 2));
             int min = Integer.valueOf(bt.substring(2, 4));
 
             String fcst_time = "";
             hour = hour + 1; min = min - 30;
-//            if(hour<10) {
-//                hour='0'+hour;
-//            }
-//            if(min<10) {
-//                min='0'+min;
-//            }
+
             String str = String.format("%02d%02d", hour, min);
             fcst_time = str;
 
-
             Log.d("예보보보보보보보보보보보보보보ㅗ보보보봅시각", fcst_time); //1960
+            int n = -1;
 
+            //배열의 크기만큼 반복하면서, category과 fcstValue의 값을 추출함
             for ( int i = 0 ; i < parse_item.length(); i++ ) {
                 JSONObject fObj = null;
-
 
                 String category = null;
                 String fcstDate= null;
                 String fcstTime= null;
                 String fcstValue= null;
 
+                //i번째 배열 할당
                 fObj = parse_item.getJSONObject(i);
 
+                //category, fcstValue의 값을 추출함
                 category = fObj.getString("category");
                 fcstDate = fObj.getString("fcstDate");
                 fcstTime = fObj.getString("fcstTime");
                 fcstValue = fObj.getString("fcstValue");
 
-
                 if(fcst_time.equals(fcstTime)) {
                     switch( category ){
                         case "T1H" : // 현재 시간 날씨
+                            //출력할 값을 저장
                             n++;
-                            saveNshow(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
+                            savemArrayList(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
                             break;
                         case "SKY" : // 하늘 상태
                             n++;
-                            saveNshow(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
+                            savemArrayList(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
                             break;
                         case "PTY" : // 강수 형태
                             n++;
-                            saveNshow(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
+                            savemArrayList(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
                             break;
                         default :
                             break;
@@ -290,21 +282,7 @@ public class WeatherApiFragment extends Fragment {
             }
         }
 
-
-
-        protected void onPostExecute(boolean t){
-            super.onPostExecute(t);
-//            dAsyncTask mat2 = new dAsyncTask(); //myAsyncTask 객체 생성
-//            mat2.execute(type2); //myAsyncTask 실행하기
-        }
-
-    }
-
-    protected class dAsyncTask extends AsyncTask<String, Void, Void>{
-        String forecastJsonStr = null;
-        ArrayList<HashMap<String, String>> mArrayList = new ArrayList<>();
-
-        protected void saveNshow(ArrayList<HashMap<String, String>> mArrayList, int n, String category, String fcstDate, String fcstTime, String fcstValue) {
+        protected void savemArrayList(ArrayList<HashMap<String, String>> mArrayList, int n, String category, String fcstDate, String fcstTime, String fcstValue) {
             HashMap<String, String> hashMap = new HashMap<>();
             hashMap.put(TAG_ID, String.valueOf(n));
             hashMap.put(TAG_CAT, category);
@@ -314,35 +292,54 @@ public class WeatherApiFragment extends Fragment {
 
             mArrayList.add(hashMap);
 
-            HashMap<String, String> outputHashMap = mArrayList.get(n);
-            String id = outputHashMap.get("id");
-            String cat = outputHashMap.get("category");
-            String date = outputHashMap.get("fcstDate");
-            String time = outputHashMap.get("fcstTime");
-            String val = outputHashMap.get("fcstValue");
-//            String s = String.format(getResources().getString(R.string.textview_message), id, cat, date, time, val);
-            String s = String.format(getResources().getString(R.string.textview_message), time, cat, val);
-            mTextView.append(s);
-            // {fcstTime=fcstTime0300, fcstValue=fcstValue22, id=id4, category=categoryT3H, fcstDate=fcstDate20200612}
-            Log.d("mArrayList :", String.valueOf(mArrayList.get(n)));
+        }
+        //end
+        protected void onPostExecute(ArrayList<HashMap<String, String>> mArrayList){
+            super.onPostExecute(mArrayList);
+            //mArrayList 출력
+            for(int i = 0; i < mArrayList.size(); i++) {
+                HashMap<String, String> outputHashMap = mArrayList.get(i);
+                String id = outputHashMap.get("id");
+                String cat = outputHashMap.get("category");
+                String date = outputHashMap.get("fcstDate");
+                String time = outputHashMap.get("fcstTime");
+                String val = outputHashMap.get("fcstValue");
+
+                String s = String.format(getResources().getString(R.string.textview_message), time, cat, val);
+                mTextView.append(s);
+
+                Log.d("mArrayList :", String.valueOf(mArrayList.get(i)));
+            }
+
+            //다음 AsyncTask 실행
+            dayTask = new BackgroundTask();
+            dayTask.execute(type2);
+//            mTextView.setText("Finished");
         }
 
 
+    }
+
+    protected class BackgroundTask extends AsyncTask<String, Void, ArrayList<HashMap<String, String>>>{
+        String forecastJsonStr = null;
+        ArrayList<HashMap<String, String>> mArrayList = new ArrayList<>();
+
         @Override
-        protected Void doInBackground(String... types) {
+        protected ArrayList<HashMap<String, String>> doInBackground(String... types) {
+
+            try { Thread.sleep(100 * 1); }
+            catch (InterruptedException e) { e.printStackTrace(); }
+
             for(String type : types) {
                 HttpURLConnection urlConnection = null; //HttpUrlConnection
                 BufferedReader reader = null;
-
-//                if(min < 30) { hour = hour-1; min = min + 30; } //19:00 / 1906 -> 1830
-//                else { min = 30;} //19:30 / 1945 -> 1930
                 try {
 
                     String nx = "53";
                     String ny = "38";
                     String dataType = "JSON";
                     String key = "8Gnkss2kI%2F%2BVSjVNBRpY9QcW53gpQ953TJoA2E%2B1260r6hNjBYOBotBXFuhE3XXfy2Hzx%2BSV9JdIsoEriCzApQ%3D%3D";
-                    String baseDate = "20200613";
+                    String baseDate = "20200614";
                     String baseTime = "2300";
                     int numOfRows = 77;
                     int pageNo = 1;
@@ -351,15 +348,7 @@ public class WeatherApiFragment extends Fragment {
                     try {
 
                         Log.d("Json Result =>", forecastJsonStr);
-                        JSONObject JsonObj = new JSONObject(forecastJsonStr);
-                        Log.d("JSON", forecastJsonStr);
-                        JSONObject parse_response = JsonObj.getJSONObject("response");
-                        JSONObject parse_body = parse_response.getJSONObject("body");
-                        JSONObject parse_items = parse_body.getJSONObject("items");
-                        //JSONArray parse_item = (JSONArray) parse_items.get("item");
-                        JSONArray parse_item = parse_items.getJSONArray("item");
-
-                        dayWeatherParsing(parse_item);
+                        dayWeatherParsing(forecastJsonStr);
 
 
                     } catch (JSONException e) {
@@ -384,7 +373,7 @@ public class WeatherApiFragment extends Fragment {
 
                 }
             }
-            return null;
+            return mArrayList;
         }
 
         private void jsonParsing(String type, HttpURLConnection urlConnection, BufferedReader reader, String nx, String ny, String baseDate, String baseTime, String dataType, int numOfRows, int pageNo, String key) throws IOException {
@@ -441,12 +430,19 @@ public class WeatherApiFragment extends Fragment {
             forecastJsonStr = buffer.toString(); //로드한 데이터 문자열 변수에 저장.
         }
 
-        private void dayWeatherParsing(JSONArray parse_item) throws JSONException {
+        private void dayWeatherParsing(String forecastJsonStr) throws JSONException {
+
+            JSONObject JsonObj = new JSONObject(forecastJsonStr);
+            JSONObject parse_response = JsonObj.getJSONObject("response");
+            JSONObject parse_body = parse_response.getJSONObject("body");
+            JSONObject parse_items = parse_body.getJSONObject("items");
+            JSONArray parse_item = parse_items.getJSONArray("item");
+
             int n = -1;
             for (int i = 0; i < parse_item.length(); i++) {
-                //JSONObject lObj = (JSONObject) parse_item.get(i);
+
                 JSONObject lObj = parse_item.getJSONObject(i);
-                //                    String status = lObj.optString("status");
+
                 String fcstDate = null;
                 String fcstTime = null;
                 String category = null;
@@ -468,29 +464,29 @@ public class WeatherApiFragment extends Fragment {
                     case "0600":
                         if ("TMN".equals(category)) { // 최저 기온
                             n++;
-                            saveNshow(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
+                            savemArrayList(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
                         }
                     case "1500":
                         if ("TMX".equals(category)) { // 최고 기온
                             n++;
-                            saveNshow(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
+                            savemArrayList(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
                         }
 
                     default:
                         switch (category) {
                             case "T3H": // 3시간 간격 기온
                                 n++;
-                                saveNshow(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
+                                savemArrayList(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
                                 break;
 
                             case "SKY": // 하늘 상태
                                 n++;
-                                saveNshow(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
+                                savemArrayList(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
                                 break;
                             case "PTY": // 강수 형태
 
                                 n++;
-                                saveNshow(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
+                                savemArrayList(mArrayList, n, category, fcstDate, fcstTime, fcstValue);
                                 break;
 
                             default:
@@ -502,6 +498,37 @@ public class WeatherApiFragment extends Fragment {
             }
 
         }
+
+        protected void savemArrayList(ArrayList<HashMap<String, String>> mArrayList, int n, String category, String fcstDate, String fcstTime, String fcstValue) {
+            HashMap<String, String> hashMap = new HashMap<>();
+            hashMap.put(TAG_ID, String.valueOf(n));
+            hashMap.put(TAG_CAT, category);
+            hashMap.put(TAG_DATE, fcstDate);
+            hashMap.put(TAG_TIME, fcstTime);
+            hashMap.put(TAG_VAL, fcstValue);
+
+            mArrayList.add(hashMap);
+        }
+
+        //end
+        protected void onPostExecute(ArrayList<HashMap<String, String>> mArrayList){
+            super.onPostExecute(mArrayList);
+            //출력
+            for(int i = 0; i < mArrayList.size(); i++){
+                HashMap<String, String> outputHashMap = mArrayList.get(i);
+                String id = outputHashMap.get("id");
+                String cat = outputHashMap.get("category");
+                String date = outputHashMap.get("fcstDate");
+                String time = outputHashMap.get("fcstTime");
+                String val = outputHashMap.get("fcstValue");
+                String s = String.format(getResources().getString(R.string.textview_message), time, cat, val);
+                mTextView.append(s);
+
+                Log.d("mArrayList :", String.valueOf(mArrayList.get(i)));
+            }
+
+        }
+
     }
 
 
